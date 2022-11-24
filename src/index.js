8be9fdf4dc5a7aaaff6839584aed042961098e60
Marcel Carvalho <48029185+carvalhomeo@users.noreply.github.com>
2022-11-24 14:01:36 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import { Kafka, logLevel } from "kafkajs";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors({ origin: "*" })); // process.env.ORIGIN
+
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.ORIGIN,
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+const kafka = new Kafka({
+  brokers: [process.env.BROKERS],
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  },
+  logLevel: logLevel.NOTHING,
+});
+
+const producer = kafka.producer();
+
+app.get("/start/:id", async (req, res) => {
+  console.log(req.params.id);
+  await producer.connect();
+  await producer.send({
+    topic: "path",
+    messages: [{ value: req.params.id }],
+  });
+  await producer.disconnect();
+  res.status(200).send({ product: req.params.id });
+});
+
+// io.on("connect", (socket) => {
+//   console.log("user connected: ", socket.id);
+
+//   socket.on("start", async (data) => {
+//     console.log("args", data);
+//   });
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log("SERVER RUNNING ON PORT ", process.env.PORT);
+});
